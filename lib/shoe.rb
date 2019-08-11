013eed537df5c13d6e5bba1dc56fe51d035261cb
Tiffany <ong.tifffany1503@gmail.com>
2019-08-11 00:30:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,16 +6,14 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << BRAND
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
   end
 
 end
